6cd8a661dcc19a4665c64a186e38414eb06852ab
Kun <wudhykunmam@gmail.com>
2026-05-20 09:34:35 +0700

    feat: add @ApiResponse to UserController endpoints

--- src/main/kotlin/com/user/crud/controller/UserController.kt
+++ src/main/kotlin/com/user/crud/controller/UserController.kt
@@ -3,6 +3,8 @@ package com.user.crud.controller
 import com.user.crud.dto.request.UpdateUserRequest
 import com.user.crud.service.UserService
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -18,14 +20,29 @@ import org.springframework.web.bind.annotation.RestController
 @Tag(name = "User", description ="User management endpoints")
 class UserController(val userService: UserService) {
 
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Success"),
+        ApiResponse(responseCode = "400", description = "Bad request"),
+        ApiResponse(responseCode = "404", description = "User not found"),
+    ])
     @PutMapping("/{id}")
     @Operation(summary = "Update user", description = "Updates username, email, or role for the given user ID.")
     fun updateUser(@PathVariable id: Long, @RequestBody @Valid request: UpdateUserRequest) = userService.updateUser(id, request)
 
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Success"),
+        ApiResponse(responseCode = "400", description = "Bad request"),
+        ApiResponse(responseCode = "404", description = "User not found"),
+    ])
     @GetMapping
     @Operation(summary = "List all users", description = "Returns all registered users")
     fun listAllUsers() = userService.listAllUsers()
 
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Success"),
+        ApiResponse(responseCode = "400", description = "Bad request"),
+        ApiResponse(responseCode = "404", description = "User not found"),
+    ])
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete user", description = "Permanently deletes a user by ID.")
     fun deleteUser(@PathVariable id: Long)= userService.deleteUser(id)
